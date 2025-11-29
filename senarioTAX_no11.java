fee482f218b36138cf3366bc6c06ec041011900a
Sampurna <sampurnaodari@gmail.com>
2025-11-29 21:36:21 +0545

    update

--- senarioTAX_no11.java
+++ senarioTAX_no11.java
@@ -8,8 +8,10 @@ import java.util.Scanner;
  */
 public class senarioTAX_no11
 {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your income: ");
         int income = sc.nextInt();
         int tax = 0;
 
@@ -33,6 +35,7 @@ public class senarioTAX_no11
             }
         }
 
-        System.out.println(tax);
+       
+        System.out.println("Your tax is: " + tax);
     }
 }
